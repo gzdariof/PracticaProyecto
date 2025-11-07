dfe12c90d0c9730f7dcec1521bcf98c5c60f83ff
gz.dariof <gz.dariof@gmail.com>
2025-11-07 16:26:27 -0500

    Agregar Bootstrasp a la aplicación

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css
+" rel="stylesheet">@yield('css')
     <title>@yield('title')</title>
 </head>
 <body>
     <div class="container">
-        
+        <a href="{{url('/')}}">Inicio</a>
+        <a href="{{url('categorias')}}">Categorías</a>
+        @yield('content')
     </div>
+    <script src="
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js
+"></script>@yield('js')
 </body>
 </html>
